4ab5bb8cfc0f5fb99eaa23bc5091e60cddc54efb
Farshad Gholami <gholamifarshad98@yahoo.com>
2018-10-21 14:24:18 +0330

    SSD has corrected

--- OpenCVSSD/ConsoleApplication1/ConsoleApplication1/main.cpp
+++ OpenCVSSD/ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -9,38 +9,41 @@ int main() {
 	if (leftImage.empty()||rightImage.empty()) {
 		cout << "Error in reading Left or right image" << endl;
 	}
-	int w;
-	int h;
-	h = leftImage.cols;
-	w = leftImage.rows;
+	int ro;
+	int co;
+	co = leftImage.cols;
+	ro = leftImage.rows;
 	Mat Disparity;
-	Disparity= MySSD(leftImage, rightImage, 6, 30,w,h);
+	leftImage.at<uchar>(ro-5, co-9) = 255;
+
+
+	Disparity= MySSD(leftImage, rightImage, 6, 30,ro,co);
 	imshow("left Image", leftImage);
 	imshow("right Image",rightImage);
 	imshow("Disparity Image", Disparity);
-	cout << "Width is ===>" << w << " and Hight is ====> " << h<<endl;
+	cout << "Width is ===>" << co << " and Hight is ====> " <<ro <<endl;
 	waitKey(0);
 
 	return 0;
 }
 
 
-Mat MySSD(Mat &left, Mat &right, int win, int MaxOffset,int w,int h) {
+Mat MySSD(Mat &left, Mat &right, int win, int MaxOffset,int ro,int co) {
 	int halfWin = int(win / 2);
 
-	Mat result(h ,w  , CV_8UC1, Scalar(255));
+	Mat result(ro,co, CV_8UC1, Scalar(255));
 
-	for (int i = halfWin; i < h - halfWin; ++i) {
-		for (int j = halfWin; j < w - halfWin ; ++j) {
-			int ssd_reserved = 35635;
+	for (int i = halfWin; i < ro - halfWin; ++i) {
+		for (int j = halfWin; j < co - halfWin; ++j) {
+			int ssd_reserved = 9999635;
 			int bestOffset = 0;
-			int offset_adjust = 255 / MaxOffset;
+			double offset_adjust = 255 / MaxOffset;
 			for (int offset = 0; offset <= MaxOffset; ++offset) {
-				 int ssd = 0;
+			    int ssd = 0;
 				int ssdTemp = 0;
 				for (int u = -halfWin; u <= halfWin; u++) {
 					for (int v = -halfWin; v <= halfWin; v++) {
-						ssdTemp = (left.at<uchar>((j + v), (i + u)) - right.at<uchar>((j + v) , (i + u - offset)));
+						ssdTemp = (left.at<uchar>((i + u), (j + v)) - right.at<uchar>((i + u) , (j + v - offset)));
 						ssdTemp = ssdTemp*ssdTemp;
 						ssd = ssd + ssdTemp;
 					}
@@ -51,7 +54,7 @@ Mat MySSD(Mat &left, Mat &right, int win, int MaxOffset,int w,int h) {
 
 				}
 			}
-			result.at<uchar>(j,i) = bestOffset* offset_adjust;
+			result.at<uchar>(i,j) = bestOffset* offset_adjust;
 		}
 	}
 	return result;
